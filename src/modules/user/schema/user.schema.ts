b5e373836ff52fe49c8e2664224bc7df60eab770
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-05-06 00:16:24 -0500

    changes: user schema

--- src/modules/user/schema/user.schema.ts
+++ src/modules/user/schema/user.schema.ts
@@ -1,22 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 import { Document } from 'mongoose'
 
 @Schema()
 export class User extends Document {
   @Prop({ required: true, unique: true })
-  @IsEmail({ message: 'This email is not valid' })
-  @IsNotEmpty({ message: 'Email is required' })
     email: string
 
   @Prop({ required: true })
-  @IsString()
-  @IsNotEmpty({ message: 'Password is required' })
     password: string
 
   @Prop({ required: true, unique: true })
-  @IsString()
-  @IsNotEmpty({ message: 'Nickname is required' })
     nickname: string
 }
 
