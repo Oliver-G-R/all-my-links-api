69ef3b9f8b8b8305f05c9a2389780d995ce1b38d
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-08-11 02:03:29 -0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import { AppModule } from './modules/app/app.module'
 async function bootstrap () {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('all-my-links-api/v1')
+  const whitelist = ['https://all-my-links-front.vercel.app/']
+
   app.enableCors({
-    origin: ['http://localhost:4000', 'https://all-my-links-front.vercel.app']
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
   })
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT || 3000)
