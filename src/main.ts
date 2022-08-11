62e833fda310fcf95b695cad7adcb02fa5118423
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2022-08-11 01:41:29 -0500

    add: origins cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './modules/app/app.module'
 async function bootstrap () {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('all-my-links-api/v1')
-  app.enableCors()
+  app.enableCors({
+    origin: ['http://localhost:4000', 'https://all-my-links-front.vercel.app']
+  })
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT || 3000)
 }
